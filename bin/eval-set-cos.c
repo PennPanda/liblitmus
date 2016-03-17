6312504a8fc3c54839150f5a1b044165f2a37199
Meng Xu <mengxu@cis.upenn.edu>
2016-03-17 16:07:51 -0400

    Evaluate lock-based design
    NB: Always recompile liblitmus library when system call table is changed
    
    Signed-off-by: Meng Xu

--- bin/eval-set-cos.c
+++ bin/eval-set-cos.c
@@ -9,7 +9,7 @@
 #include "litmus.h"
 #include "common.h"
 
-#define DEBUG   1
+//#define DEBUG   1
 
 #define MHZ     (2299.871)
 #define HZ      (MHZ * 1000 * 1000)
@@ -30,7 +30,7 @@ void print_latency(cycles_t *latency, int times)
 {
     int i;
 
-    printf("iter\tcycles\tus\n");
+    //printf("iter\tcycles\tus\n");
     for ( i = 0; i < times; i++ )
     {
         printf("%d\t%lld\t%.3f\n", i, (unsigned long long) latency[i], latency[i] * 1.0 / MHZ);
@@ -72,6 +72,7 @@ int main (int argc, char* argv[])
     cycles_t* latency;
     int (*set_cos) (uint32_t cos_id, uint32_t val, cycles_t *start, cycles_t *end);
     cpu_set_t cpuset;
+    struct sched_param sched_param;
 
     ret = 0;
 
@@ -97,8 +98,10 @@ int main (int argc, char* argv[])
     times = atoi(argv[5]);
     is_print = atoi(argv[6]);
 
+#ifdef DEBUG
     printf("Type:%d s_cpu_id:%d cos_id:%d cos_val:%x times:%d\n",
             type, s_cpu_id, cos_id, cos_val, times);
+#endif
 
     if ( cos_id < 0 || cos_id >= 4 )
     {
@@ -124,8 +127,20 @@ int main (int argc, char* argv[])
     /* set cpu affinity */
     CPU_ZERO(&cpuset);
     CPU_SET(s_cpu_id, &cpuset);
-    sched_setaffinity(0, sizeof(cpuset), &cpuset);
+    if ( sched_setaffinity(0, sizeof(cpuset), &cpuset) )
+    {
+        fprintf(stderr, "setaffinity fails. exit\n");
+        return 3;
+    }
 
+    /* set sched to SCHED_FIFO and highest priority */
+    memset(&sched_param, 0, sizeof(struct sched_param));
+    sched_param.sched_priority = 99; /* highest priority in FIFO */
+    if ( sched_setscheduler(0, SCHED_FIFO, &sched_param) )
+    {
+        fprintf(stderr, "setscheduler fails. exit\n");
+        return 4;
+    }
     
     for ( i = 0; i < times; i++ )
     {
