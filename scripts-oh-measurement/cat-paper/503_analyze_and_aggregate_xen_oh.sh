ffc53fe7a84af6d9be5f18d0efc1687ddb19bd21
Meng Xu <mengxu@cis.upenn.edu>
2016-10-05 13:49:20 -0400

    script oh

---
+++ scripts-oh-measurement/cat-paper/503_analyze_and_aggregate_xen_oh.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source 00_env.sh
+source 01_range.sh
+
+declare -a Algs=( "GSN-EDF" )
+#declare -a TYPES=( "Uniform_Light" "Uniform_Medium" "Uniform_Heavy" )
+declare -a TYPES=( "Uniform_Light" )
+DUR=30 #seconds
+WAIT=1 #release tasks at the same time?
+#IS_CAT=1 #evaluate on CAT?
+RTTASK=ca_spin_v3_oh_measurement
+ROOT=/home/pennpanda/github/liblitmus-precise/scripts-oh-measurement/cat-paper
+TRACEBIN_ROOT=${ROOT}/trace_bin
+TRACECSV_ROOT=${ROOT}/trace_csv
+TRACEOH_ROOT=${ROOT}/trace_oh
+
+declare -a EVENTS=( "CXS_START" "SCHED_START" "SCHED2_START" "RELEASE_START" "RELEASE_LATENCY" )
+
+for event in "${EVENTS[@]}"
+do
+for ((case=${case_min}; case<=${case_max}; case+=${case_step}));do
+for type in "${TYPES[@]}"
+do
+	for alg in "${Algs[@]}"
+	do
+		for ((num_tasks=${num_tasks_min}; num_tasks<=${num_tasks_max}; num_tasks+=${num_tasks_step}));do
+            # Generate input parameters
+            csv_folder=${TRACECSV_ROOT}/numtasks${num_tasks}/xen
+            input_csv_file=${csv_folder}/xentrace_test${case}_dur${DUR}_wait${WAIT}_sched${alg}_type${type}_numtasks${num_tasks}_rt${RTTASK}_xen_env${ENV}-all.csv
+            output_oh_folder=${TRACEOH_ROOT}/numtasks${num_tasks}/xen
+            if [[ ! -d ${output_oh_folder} ]]; then
+                echo "mkdir -p ${output_oh_folder}"
+                mkdir -p ${output_oh_folder}
+            fi
+            output_do_sched=${output_oh_folder}/xentrace_test${case}_dur${DUR}_wait${WAIT}_sched${alg}_type${type}_numtasks${num_tasks}_rt${RTTASK}_xen_env${ENV}-do_sched.csv
+            output_cxt_switch=${output_oh_folder}/xentrace_test${case}_dur${DUR}_wait${WAIT}_sched${alg}_type${type}_numtasks${num_tasks}_rt${RTTASK}_xen_env${ENV}-cxt_switch.csv
+            output_cxt_saved=${output_oh_folder}/xentrace_test${case}_dur${DUR}_wait${WAIT}_sched${alg}_type${type}_numtasks${num_tasks}_rt${RTTASK}_xen_env${ENV}-cxt_saved.csv
+            echo "python ./analyze_xentrace_csv2result.py ${input_csv_file} ${output_do_sched} ${output_cxt_switch} ${output_cxt_saved}"
+            python ./analyze_xentrace_csv2result.py ${input_csv_file} ${output_do_sched} ${output_cxt_switch} ${output_cxt_saved}
+		done
+	done
+done
+done
+done
